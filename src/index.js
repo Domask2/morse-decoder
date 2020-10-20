2fbc3e268482182d85b021f208a331a47f84733e
Domask <domask2@mail.ru>
2020-10-20 11:06:40 +0400

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let exprArr = expr.match(/.{1,10}/g);
+  let resultMorse = [];
+
+  for (const item of exprArr) {
+    if (item === "**********") {
+      resultMorse.push(' ');
+      continue;
+    }
+
+    let morse = '';
+    let newItem = item.match(/.{1,2}/g)
+    for (const el of newItem) {
+      if (el == "10") morse += ".";
+      if (el == "11") morse += "-";
+    }
+    resultMorse.push(morse);
+     
+  }
+  
+  let res = '';
+  resultMorse.map(item=> {
+    item === ' ' ? res+= ' ': res+=MORSE_TABLE[item]
+     
+  })
+
+ return res;
 }
 
 module.exports = {
